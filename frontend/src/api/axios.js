cbaf263828a1aec17c24ca377f934d2c68e37f5f
Krrish Kumar <imkrrishgupta@gmail.com>
2026-02-27 22:33:16 +0530

    fix API

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,10 +2,9 @@ import axios from "axios";
 
 const baseURL = import.meta.env.VITE_BASE_URL + "/api/v1";
 
-const axiosInstance = axios.create({
+const API = axios.create({
   baseURL,
   withCredentials: true
 });
 
-export { axiosInstance };
-export default axiosInstance;
+export default API;
