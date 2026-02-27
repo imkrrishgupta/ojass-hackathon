f3e04a58ea5c6419b00057441e65ccc6f48ba991
Krrish Kumar <imkrrishgupta@gmail.com>
2026-02-27 13:19:28 +0530

    fix ApiResponse.js

--- backend/src/utils/ApiResponse.js
+++ backend/src/utils/ApiResponse.js
@@ -1,23 +1,10 @@
-class ApiError extends Error {
-    constructor(
-        statusCode,
-        message = "Something went wrong",
-        errors = [],
-        stack = ""
-    ){
-        super(message)
-        this.statusCode = statusCode
-        this.data = null
+class ApiResponse {
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode,
+        this.data = data,
         this.message = message
-        this.success = false
-        this.errors = errors;
-
-        if (stack){
-            this.stack = stack;
-        }else{
-            Error.captureStackTrace(this, this.constructor)
-        }
+        this.success = statusCode < 400
     }
 }
 
-export { ApiError };
+export { ApiResponse };
